51ef7074a9af6e72954360af4d31f9ede8f9ae5a
mutawallihasim <mutawallihasim31@gmail.com>
2026-05-10 19:05:04 +0800

    commit pertama

---
+++ connection.php
@@ -0,0 +1,17 @@
+<?php
+$server = "localhost" ;
+$username = "root";
+$password = "";
+$db = "LokalThrift";
+
+$conn = new mysqli($server, $username, $password, $db);
+
+if ($conn->connect_error) {
+    die("Koneksi Gagal". $conn->connect_error);
+} else {
+    echo "sjdodosjdos";
+    //header("Location: dashboard.php");
+    //exit();
+}
+
+?>
